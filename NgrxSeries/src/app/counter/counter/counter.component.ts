015d048c4cc4f180d3c99bb9d179fff183295af2
priyankasaini <priyankasaini0987@gmail.com>
2023-03-23 13:32:01 +0530

    counter function without ngrx

---
+++ NgrxSeries/src/app/counter/counter/counter.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  templateUrl: './counter.component.html',
+  styleUrls: ['./counter.component.css']
+})
+export class CounterComponent {
+
+  counter: number = 0;
+
+  onIncrement(){
+    this.counter++;
+  }
+
+  onDecrement(){
+    this.counter--;
+  }
+
+  onReset(){
+    this.counter = 0;
+  }
+
+}
